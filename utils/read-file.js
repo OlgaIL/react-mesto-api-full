8d1dda653639376344085be5a102ff4bd496e51f
Olga Ilyina <kukushkina@yandex.ru>
2020-11-07 00:31:57 +0300

    Проектная работа 13. удалены лишние файлы 2

--- utils/read-file.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs').promises;
-// const { parse } = require('querystring');
-
-module.exports = (url) => fs.readFile(url, { encoding: 'utf8' })
-  .then((file) => JSON.parse(file));
